76ee4198daf8b8ce9e4f7b15fd419508f7f7f42d
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-21 16:45:59 +0900

    validation編集

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,6 @@
 class Channel < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
-  validates :is_private, presence: true
+  validates :status, presence: true
   validates :description, length: { maximum: 1000 }
-  validates :created_user_id, presence: true
+  validates :author_id, presence: true
 end
