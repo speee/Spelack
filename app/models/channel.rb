2e100163dd598f8755ca90ad364be1da83d1aeec
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-21 12:28:42 +0900

    add validates

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,6 @@
 class Channel < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :is_private, presence: true
+  validates :description, length: { maximum: 1000 }
+  validates :created_user_id, presence: true
 end
