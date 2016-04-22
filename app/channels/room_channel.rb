5caba21e073fba0cee8a5364375b38767b14ab9c
MKenta <atto.mark56@gmail.com>
2016-04-22 15:59:12 +0900

    queを利用しない方法に変更

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,5 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
-  def speak
+    stream_from "messages"
   end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,10 @@ class RoomsController < ApplicationController
   def show
     @messages = Message.all
   end
+  def create
+    ActionCable.server.broadcast "messages",
+      body: params[:message][:body],
+      username: params[:message][:username]
+    head :ok
+  end
 end
