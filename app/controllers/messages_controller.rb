c54361419b63f0342a2d014ca2e2e6ad44591193
MKenta <atto.mark56@gmail.com>
2016-04-22 18:33:26 +0900

    rubocop修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,9 @@
 class MessagesController < ApplicationController
   def index
   end
+
   def create
-    ActionCable.server.broadcast "messages",
+    ActionCable.server.broadcast 'messages',
       body: params[:message][:body],
       username: params[:message][:username]
     head :ok
