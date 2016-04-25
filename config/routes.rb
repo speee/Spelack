5a8246652a0d660e4603d4caf8a43916dd83ee26
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-25 12:38:57 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'channels#index'
   devise_for :users
-  resources :channels, only: [:index, :new, :create, :show]
+  resources :channels, except: [:destroy]
 end
