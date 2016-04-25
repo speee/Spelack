b119391861247159944e3b419126785e108d0e61
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-25 12:53:23 +0900

    チャンネル編集機能作成

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,5 +1,5 @@
 class ChannelsController < ApplicationController
-  before_action :set_channel, only: [:show]
+  before_action :set_channel, only: [:show, :edit, :update]
   def index
     @channels = Channel.all
   end
@@ -20,6 +20,13 @@ class ChannelsController < ApplicationController
   def show
   end
 
+  def update
+    if @channel.update(channel_params)
+      redirect_to channel_path(@channel)
+    else
+      render :edit
+    end
+  end
   private
 
   def set_channel
