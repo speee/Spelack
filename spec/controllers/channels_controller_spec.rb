9aa92c79b79f1862f4df15326f246b67eacae9ab
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-04-26 18:15:23 +0900

    テスト修正

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -18,14 +18,15 @@ RSpec.describe ChannelsController do
     end
   end
   describe 'GET #index' do
-    let(:channel) { create(:channel) }
+    let(:channels) { create_list(:channel, 25) }
     context 'when there is a search word' do
-      before { get :index, query: channel.name }
-      specify { expect(assigns(:channels)).to include channel }
+      before { get :index, query: channels[5].name }
+      specify { expect(assigns(:channels)).to include channels[5] }
       it_behaves_like 'http_succses', :index
     end
 
     context 'when the search word does not exist' do
+      specify { expect(assigns(:channels)).to eq channels }
       before { get :index }
       it_behaves_like 'http_succses', :index
     end
@@ -50,17 +51,17 @@ RSpec.describe ChannelsController do
     let(:channel) { create(:channel) }
     let(:name) { FFaker::BaconIpsum.characters(10) }
     context 'when updating channel is success' do
-      before { patch :update, id: channel, channel: attributes_for(:channel) }
       it 'locates the requersted channel' do
+        patch :update, id: channel, channel: attributes_for(:channel)
         expect(assigns(:channel)).to eq channel
         expect(response).to have_http_status(302)
         expect(response).to redirect_to channel_path(assigns(:channel).id)
       end
 
       it 'changes channels attributes' do
-        expect do
-          patch :update, id: channel, channel: attributes_for(:channel, name: name)
-        end.to change { assigns(:channel).name }.from(channel.name).to(name)
+        patch :update, id: channel, channel: attributes_for(:channel, name: name)
+        channel.reload
+        expect(channel.name).to eq name
       end
     end
     context 'when updating channel is not success' do
