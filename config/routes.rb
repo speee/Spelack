c77bda1c8b422e0c7a7eb69f9402a5092a6968e1
YasuomiTakano <yasuomi.takano@speee.jp>
2016-04-26 18:03:12 +0900

    rubocop対応

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: '/letter_opener'
-  end
-
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
