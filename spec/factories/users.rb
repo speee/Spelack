e4a8115d212cf5294a526fc67d2a05c3e6326060
YasuomiTakano <yasuomi.takano@speee.jp>
2016-04-27 15:48:22 +0900

    rubocop対応

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,10 @@
 FactoryGirl.define do
-
   factory :user do
-    email                 "kkk@gmail.com"
-    password              "00000000"
-    password_confirmation "00000000"
-    nickname              "abe"
-    first_name            "abc"
-    last_name             "abc"
+    email                 'kkk@gmail.com'
+    password              '00000000'
+    password_confirmation '00000000'
+    nickname              'abc'
+    first_name            'abc'
+    last_name             'abc'
   end
-
 end
