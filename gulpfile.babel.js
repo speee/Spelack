4fbba0e083429ef2a47a82922fe1bc357fe165ca
MKenta <atto.mark56@gmail.com>
2016-04-27 11:02:51 +0900

    edit gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp'
 import webpack from 'gulp-webpack'
 import webpackConfig from './webpack.config.js';
-require('babel-register')
 gulp.task('webpack', () => {
   gulp.src('app/assets/javascripts/**/*.js')
     .pipe(webpack(webpackConfig))
