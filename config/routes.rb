ca630c1ba817c3e3169e174b30d5889d32769ea9
MKenta <atto.mark56@gmail.com>
2016-05-02 19:03:04 +0900

    add api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+  get 'messages/index'
+  end
+  namespace 'api' do
+    resources :messages, only: :index
+  end
   resources :messages, only: %i(index create)
   devise_for :users
   mount ActionCable.server => '/cable'
