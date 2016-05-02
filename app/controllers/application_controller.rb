1fddc1bad2788cce3637ed10fcdc4f2acf71a32b
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-02 17:40:07 +0900

    テストのためログイン後のパス設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :middle_name, :last_name, :nickname])
   end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in' # サインアウト後のリダイレクト先URL
+  end
+
 end
