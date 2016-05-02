c4ed28cd907353db633f4325e37790a561ff3ce5
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-02 18:42:38 +0900

    rubocop対応

--- spec/supports/controller_macros.rb
+++ spec/supports/controller_macros.rb
@@ -1,7 +1,7 @@
 module ControllerMacros
   def login_user
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       sign_in FactoryGirl.create(:user)
     end
   end
