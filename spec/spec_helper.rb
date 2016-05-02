bf6b7674b1aa6ac72963825c403a6033eac21a31
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-02 18:39:52 +0900

    Deviseのincludeエラーの解消

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-  config.extend ControllerMacros, :type => :controller
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
