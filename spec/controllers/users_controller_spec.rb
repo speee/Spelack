03747c046a1b4840ef1329c7aa837863b297fe78
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-06 15:05:49 +0900

    質問するため現状をcommit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,8 +5,26 @@ describe UsersController do
     specify { expect(response).to have_http_status(:ok) }
     specify { expect(response).to render_template template }
   end
-  describe 'GET #index' do
-    before { get :index }
-    it_behaves_like 'http_succses', :index
+
+  describe 'login user' do
+    describe 'GET #index' do
+      before { get :index }
+
+      # 1. ログインできなかった際にログインページにリダイレクトされるか (0%)
+      context '???' do
+      end
+
+      # 2. ログインできた時にindexページに遷移するか (100%)
+      context 'by login user' do
+        it_behaves_like 'http_succses', :index
+      end
+
+      # 3. ログインしていない状態で他のページに遷移しようとしたらログインページにリダイレクト (0%)
+      context '???' do
+      end
+    end
   end
+
+
+
 end
