f2764336f28acb3af2aa6a2a669d8b388c390fbf
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-09 12:18:47 +0900

    コントローラーの不要なテストを削除

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,29 +9,11 @@ describe UsersController do
   login_user
   describe 'login user' do
     describe 'GET #index' do
-      # 1. ログインできなかった際にログインページにリダイレクトされるか
-      context '???1' do
-        subject do
-          logout(:user)
-          @user = User.new(email: nil)
-          sign_in @user
-        end
-        specify { expect(subject).to redirect_to(new_user_session_path) }
-      end
 
-      # 2. ログインできた時にindexページに遷移するか
+      # ログインできた時にindexページに遷移するか
       context 'by login user' do
         before { get :index }
         it_behaves_like 'http_succses', :index
-      end
-
-      # 3. ログインしていない状態で他のページに遷移しようとしたらログインページにリダイレクトされる
-      context '???3' do
-        subject do
-          logout(:user)
-          # before { get :index }
-        end
-        specify { expect(subject).to redirect_to(new_user_session_path) }
       end
 
       context 'should have a current_user' do
