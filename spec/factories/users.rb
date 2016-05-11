60ac1f63dc9025ce84c050957e5b4933edc8725e
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-11 11:50:37 +0900

    FFakerの対応

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
   factory :user do
+    password = FFaker::Internet.password(8)
     email                 { FFaker::Internet.email }
-    password              '00000000'
-    password_confirmation '00000000'
-    nickname              'abc'
-    first_name            'abc'
-    last_name             'abc'
+    password              password
+    password_confirmation password
+    nickname              { FFaker::Name.name }
+    first_name            { FFaker::Name.first_name }
+    last_name             { FFaker::Name.last_name }
   end
 end
