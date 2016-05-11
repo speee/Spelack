d1ad4b65ec5dbb8ab373aa06823a44709cd6ea79
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-11 10:09:06 +0900

    itの明確化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,37 +4,37 @@ RSpec.describe User do
   describe 'validations' do
     subject { build(:user) }
     describe 'nickname' do
-      context 'when it is empty' do
+      context 'when nickname is empty' do
         it { is_expected.to be_invalid_on(:nickname).with(nil) }
       end
 
-      context 'when it is over the max characters' do
+      context 'when nickname is over the max characters' do
         it { is_expected.to be_invalid_on(:nickname).with('a' * 21) }
       end
     end
 
     describe 'first_name' do
-      context 'when it is empty' do
+      context 'when first_name is empty' do
         it { is_expected.to be_invalid_on(:first_name).with(nil) }
       end
 
-      context 'when it is over the max characters' do
+      context 'when first_name is over the max characters' do
         it { is_expected.to be_invalid_on(:first_name).with('a' * 31) }
       end
     end
 
     describe 'middle_name' do
-      context 'when it is over the max characters' do
+      context 'when middle_name is over the max characters' do
         it { is_expected.to be_invalid_on(:middle_name).with('a' * 31) }
       end
     end
 
     describe 'last_name' do
-      context 'when it is empty' do
+      context 'when last_name is empty' do
         it { is_expected.to be_invalid_on(:last_name).with(nil) }
       end
 
-      context 'when it is over the max characters' do
+      context 'when last_name is over the max characters' do
         it { is_expected.to be_invalid_on(:last_name).with('a' * 31) }
       end
     end
