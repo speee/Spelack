43e499d84d5cf066e429cd131c01ed9853ba24af
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-12 17:41:09 +0900

    default_avatar.pngをpublic以下に移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ActiveRecord::Base
   validates :middle_name, length: { maximum: 30 }
   validates :last_name, presence: true, length: { maximum: 30 }
 
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: "/public/image/default_avatar.png"
   validates_attachment :avatar, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
 end
