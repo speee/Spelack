2695c6ca2421404d700e97e96a1441aa4e719d7c
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-12 12:30:33 +0900

    user.rbの不要なコメントアウトの削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,16 +10,4 @@ FactoryGirl.define do
     confirmation_sent_at  { Time.zone.now }
     confirmation_token    '9zdxfHWNyJXDGqKCwNn5'
   end
-
-  # factory :user do
-  #   email                 'foo@example.com'
-  #   password              '00000000'
-  #   password_confirmation '00000000'
-  #   nickname              'abc'
-  #   first_name            'abc'
-  #   last_name             'abc'
-  #   confirmed_at          { Time.zone.now }
-  #   confirmation_sent_at  { Time.zone.now }
-  #   confirmation_token    '9zdxfHWNyJXDGqKCwNn5'
-  # end
 end
