66b26717dbc230a46756963e7f550f49a7f7a6f2
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-13 13:48:23 +0900

    ローティング設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
-  resources :channels
+  resources :channels do
+    member do
+      get 'join'
+    end
+
+    collection do
+      get 'search'
+    end
+  end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
