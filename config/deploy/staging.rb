ac140cbd37f4639c2a3120cc1f45c2f647d29167
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-16 16:28:12 +0900

    設定修正

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :stage, :staging
-set :rails_env, 'production'
+set :rails_env, 'staging'
 server '52.193.153.0', user: 'spelack-dev', roles: %w(app web db)
 set :ssh_options, keys: '~/.ssh/aws_rsa'
