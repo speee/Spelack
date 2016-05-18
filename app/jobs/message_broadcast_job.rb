090240fb67ba2e5ea8226cffbfc7b859c10de410
MKenta <atto.mark56@gmail.com>
2016-05-18 12:21:26 +0900

    job内容変更(virtual attributeによる)

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,9 +2,9 @@ class MessageBroadcastJob < ActiveJob::Base
   queue_as :default
 
   def perform(message)
-    message_hash = JSON.parse(message.to_json)
-    message_hash["nickname"] = message.user.nickname
+    message_hash = message.to_json
+    #message_hash['nickname'] = message.user.nickname
     ActionCable.server.broadcast 'messages',
-      message_hash.to_json
+      message_hash.to_json(methods: :nickname)
   end
 end
