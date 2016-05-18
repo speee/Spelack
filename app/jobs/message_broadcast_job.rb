f166bc09229ad88df0e996a37762a7c33ed70435
MKenta <atto.mark56@gmail.com>
2016-05-18 12:22:25 +0900

    remove comment

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,9 +2,7 @@ class MessageBroadcastJob < ActiveJob::Base
   queue_as :default
 
   def perform(message)
-    message_hash = message.to_json
-    #message_hash['nickname'] = message.user.nickname
     ActionCable.server.broadcast 'messages',
-      message_hash.to_json(methods: :nickname)
+      message.to_json(methods: :nickname)
   end
 end
