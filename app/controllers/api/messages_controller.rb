426b727f668060cfcf1e916f7f55b1e528afc1bf
MKenta <atto.mark56@gmail.com>
2016-05-18 12:20:38 +0900

    virtual attributeによるnickname表示

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.all
-    render json: @messages
+    messages = Message.all
+    render json: messages.to_json(methods: :nickname)
   end
 end
