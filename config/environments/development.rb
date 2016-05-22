e3bf8123b9f514bc927b3b78875ea2a119d3130d
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-22 23:25:19 +0900

    smtp設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => 'wataru.yamaura@speee.jp',
+    :password => 'Ka5notre321',
+    :authentication => 'login',
+    :openssl_verify_mode  => 'none'
+  }
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
