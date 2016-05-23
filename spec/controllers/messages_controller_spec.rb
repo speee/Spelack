b224e303fb234cab1863432c0115411708b1584d
MKenta <atto.mark56@gmail.com>
2016-05-23 14:58:45 +0900

    add controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe SearchChannelsController do
+RSpec.describe MessagesController do
   login_user
-  shared_examples_for 'http_succses' do |template|
-    specify { expect(response).to have_http_status(:ok) }
-  end
   describe 'POST #create' do
     context 'when saving message is success' do
       before { post :create, message: attributes_for(:message) }
-      it { change(Channel, :count).by(2) }
-      it { change(assigns(:channel).users, :count).by(2) }
-      specify { expect(response).to have_http_status(302) }
-      specify { expect(response).to redirect_to channel_path(assigns(:channel)) }
+      it { change(Message, :count).by(1) }
+      specify { expect(response).to have_http_status(:ok) }
     end
-    context 'when saving channel is not success' do
-      before { post :create, channel: attributes_for(:channel, name: nil) }
-      it_behaves_like 'http_succses', :new
+    context 'when saving message is not success' do
+      before { post :create, message: attributes_for(:message, text: nil) }
+      it 'redirects to message' do
+        expect(response).to have_http_status(302)
+      end
+      it 'renders to #index' do
+        expect(response).to render_template(:index)
+      end
     end
   end
 end
