cab4f0b2ffab144b73d6881a034415eccaacf291
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-23 17:13:43 +0900

    mailer設定追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,9 @@
-class ApplicationMailer < ActionMailer::Base
-     default from: ENV['EMAIL_USER']
+class ApplicationMailer < Devise::Mailer
+  before_action :default_opts
+
+  private
+
+  def default_opts
+    default from: ENV['EMAIL_USER']
+  end
 end
