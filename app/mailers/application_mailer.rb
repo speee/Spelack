c98ad7b800ac892dfe5568f8bee5fc6d1cbdfb8f
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-23 16:43:10 +0900

    試験コミット

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  after_action :set_envelope_from
-
-  private
-
-  def set_envelope_from
-    mail.smtp_envelope_from = ENV['EMAIL_USER']
-  end
+     default from: ENV['EMAIL_USER']
 end
