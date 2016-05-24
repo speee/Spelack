2b8897b21cade7883efca9d918e4a77abe346990
MKenta <atto.mark56@gmail.com>
2016-05-24 15:35:04 +0900

    delete句追加

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -27,16 +27,19 @@ RSpec.describe MessagesController do
       expect(assigns(:message)).to eq message
       expect(response).to have_http_status(:ok)
     end
-
     it 'changes messages attributes' do
       patch :update, id: message, message: attributes_for(:message, text: text)
       message.reload
       expect(message.text).to eq text
     end
   end
-
   context 'when updating message is not success' do
     before { patch :update, id: message, message: attributes_for(:message, text: nil) }
     specify { expect(response).to have_http_status(:bad_request) }
+  end
+  describe 'DELETE #destroy' do
+    let(:message) { create(:message) }
+    before { delete :destroy, id: message }
+    it { change(Message, :count).by(-1) }
   end
 end
