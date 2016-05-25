35acb7d2298c40b7e9fec82c78eb763c7897b778
MKenta <atto.mark56@gmail.com>
2016-05-25 12:24:13 +0900

    fix indent

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -18,24 +18,25 @@ RSpec.describe MessagesController do
       end
     end
   end
-  describe 'PATCH #update'
-  let(:message) { create(:message) }
-  let(:text) { FFaker::BaconIpsum.characters(10) }
-  context 'when updating message is success' do
-    it 'locates the requersted message' do
-      patch :update, id: message, message: attributes_for(:message)
-      expect(assigns(:message)).to eq message
-      expect(response).to have_http_status(:ok)
+  describe 'PATCH #update' do
+    let(:message) { create(:message) }
+    let(:text) { FFaker::BaconIpsum.characters(10) }
+    context 'when updating message is success' do
+      it 'locates the requersted message' do
+        patch :update, id: message, message: attributes_for(:message)
+        expect(assigns(:message)).to eq message
+        expect(response).to have_http_status(:ok)
+      end
+      it 'changes messages attributes' do
+        patch :update, id: message, message: attributes_for(:message, text: text)
+        message.reload
+        expect(message.text).to eq text
+      end
     end
-    it 'changes messages attributes' do
-      patch :update, id: message, message: attributes_for(:message, text: text)
-      message.reload
-      expect(message.text).to eq text
+    context 'when updating message is not success' do
+      before { patch :update, id: message, message: attributes_for(:message, text: nil) }
+      specify { expect(response).to have_http_status(:bad_request) }
     end
-  end
-  context 'when updating message is not success' do
-    before { patch :update, id: message, message: attributes_for(:message, text: nil) }
-    specify { expect(response).to have_http_status(:bad_request) }
   end
   describe 'DELETE #destroy' do
     let(:message) { create(:message) }
