662ef5bc4b66f13804bae92ce7c9a336c0f71327
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-26 08:19:26 +0900

    robocop_todo修正

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.session_store :redis_store, servers: 'redis://spelackec-001.apvows.0001.apne1.cache.amazonaws.com:6379/0', expire_in: 1.day
+  config.session_store :redis_store,
+      servers: 'redis://spelackec-001.apvows.0001.apne1.cache.amazonaws.com:6379/0',
+      expire_in: 1.day
   config.assets.js_compressor = :uglifier
   Rails.application.config.action_cable.allowed_request_origins = ['http://52.196.227.190']
   config.action_mailer.default_url_options = { host: '52.196.227.190' }
