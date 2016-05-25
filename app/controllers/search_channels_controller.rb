d5cc0314e2507ecaecbe51d0dddfe1f2a5123749
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-25 11:26:44 +0900

    channel検索でjoinしているchannelを下に表示する機能の実装

--- app/controllers/search_channels_controller.rb
+++ app/controllers/search_channels_controller.rb
@@ -8,5 +8,16 @@ class SearchChannelsController < ApplicationController
 
   def search_channels
     @channels = Channel.search(params[:query])
+    @joined_channels = []
+    @not_joined_channels = []
+    @channels.each do |channel|
+      # binding.pry
+      if current_user.channels.exists?(channel.id) then
+        @joined_channels << channel
+      else
+        @not_joined_channels << channel
+      end
+    end
   end
+
 end
