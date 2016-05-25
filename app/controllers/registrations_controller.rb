fd78449def46fbb7039ebcafbb20012aad148a30
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-25 16:47:45 +0900

    ign up後のページ遷移 実装

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,14 +6,4 @@ class RegistrationsController < Devise::RegistrationsController
     def after_sign_up_path_for(resource)
       '/users/after_sign_up'
     end
-
-    # # メール確認完了後の遷移先
-    # def after_confirmation_path_for(resource_name, resource)
-    #   '/users/after_sign_up'
-    # end
-
-    # # 認証メールを再送信した後の遷移先
-    # def after_resending_confirmation_instructions_path_for(resource_name)
-    #   '/users/after_sign_up'
-    # end
 end
