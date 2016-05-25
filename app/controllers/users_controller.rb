f73c561bf93383a4a9b368103375c4204033724a
YasuomiTakano <yasuomi.takano@speee.jp>
2016-05-25 20:44:00 +0900

    動画の読み込み方法の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def index
-    # asset_path('top_movie.mp4')
-    @movie_path = 'top_movie.mp4'
+    # @movie_path = 'top_movie.mp4'
   end
 
   def after_sign_up
