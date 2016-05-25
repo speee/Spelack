9b17df4ced33eefeb142ea54f4c0929893cfa8b8
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-25 11:12:04 +0900

    SMTP設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   config.eager_load = true
   config.action_mailer.default_url_options = { host: '52.192.80.168' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_options = { from: ENV['EMAIL_USER'] }
   config.action_mailer.smtp_settings = {
@@ -12,8 +13,9 @@ Rails.application.configure do
     domain: 'smtp.gmail.com',
     user_name: ENV['EMAIL_USER'],
     password: ENV['EMAIL_PASSWORD'],
+    from: ENV['EMAIL_USER'],
     authentication: 'login',
-    openssl_verify_mode: 'none',
+    openssl_verify_mode: 'none'
   }
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
