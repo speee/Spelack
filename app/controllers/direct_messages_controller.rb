6f33db46503fca449ea201728de9aba4b969f95f
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-26 19:02:16 +0900

    rubocop対応

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,12 +1,12 @@
 class DirectMessagesController < ApplicationController
   before_action :search_users, only: [:index]
   before_action :set_dm_channels, only: [:index, :create]
-  before_action :set_params, only:[:create]
+  before_action :set_params, only: [:create]
   def index
   end
 
   def create
-    if @dm_channels.find {|n| n[:name] == @dm_params[:name]}
+    if @dm_channels.find { |n| n[:name] == @dm_params[:name] }
       redirect_to channels_path
     else
       @channel = Channel.new(@dm_params)
@@ -15,20 +15,21 @@ class DirectMessagesController < ApplicationController
         @user.channels << @channel
         redirect_to channel_path(@channel)
       else
-          render :index
+        render :index
       end
     end
   end
 
   private
+
   def set_params
     user = params.require(:user).permit(:user_id)
     @user = User.find(user[:user_id])
-    @channel_name = [@user.nickname,current_user.nickname].sort.join('-')
+    @channel_name = [@user.nickname, current_user.nickname].sort.join('-')
     @dm_params = {
       name: @channel_name,
       status: 2,
-      author_id: current_user.id
+      author_id: current_user.id,
     }
   end
 
