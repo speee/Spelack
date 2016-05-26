d9e6f8ec53c399f26ab342c302eb5576c06a3a9b
WataruYamaura2 <wataru.yamaura@speee.jp>
2016-05-26 15:39:03 +0900

    users_controllerのみログインを必要としないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :after_sign_up]
   def index
   end
 
